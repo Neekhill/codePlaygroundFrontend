875f556b5995a118121fc7bcee49e2f82e5c6038
nikhil yadav <iamnikhilyadav27@gmail.com>
2022-01-12 17:38:44 +0530

    connected backend with frontend and executed code

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import styled from "styled-components";
+import { useState } from "react";
+import axios from "axios";
 
 const Title = styled.h1`
-  margin: 2rem;
+  margin: 1rem 2rem;
 `;
 const Conatiner = styled.div`
   display: flex;
@@ -10,11 +12,13 @@ const CodeArea = styled.textarea`
   margin-left: 2rem;
   font-size: 20px;
   flex: 2;
+  min-width: 700px;
+  padding: 1rem;
 `;
 const OutputArea = styled.div`
   flex: 1;
   margin: 0 20px;
-  padding: 5px;
+  padding: 1rem;
   border: 1px solid black;
 `;
 const SubmitButton = styled.button`
@@ -25,14 +29,34 @@ const SubmitButton = styled.button`
   cursor: pointer;
 `;
 function App() {
+  const [code, setCode] = useState("");
+  const [output, setOutput] = useState("");
+
+  const handleSubmit = async () => {
+    const payload = {
+      language: "cpp",
+      code: code,
+    };
+    try {
+      const result = await axios.post("http://localhost:3030/run", payload);
+      setOutput(result.data.output);
+    } catch (err) {
+      setOutput(err.response.data.err.stderr);
+    }
+  };
   return (
     <>
       <Title>Code Playground</Title>
       <Conatiner>
-        <CodeArea rows="22" cols="100"></CodeArea>
-        <OutputArea></OutputArea>ˀ
+        <CodeArea
+          rows="22"
+          cols="100"
+          value={code}
+          onChange={(e) => setCode(e.target.value)}
+        ></CodeArea>
+        <OutputArea>{output}</OutputArea>ˀ
       </Conatiner>
-      <SubmitButton>Run</SubmitButton>
+      <SubmitButton onClick={handleSubmit}>Run</SubmitButton>
     </>
   );
 }
