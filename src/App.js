c161aba2df45152f5eb3903aba573e21181958f0
nikhil yadav <iamnikhilyadav27@gmail.com>
2022-01-12 16:00:06 +0530

    basic bocode playground structure ready

--- src/App.js
+++ src/App.js
@@ -1,7 +1,40 @@
 import styled from "styled-components";
 
+const Title = styled.h1`
+  margin: 2rem;
+`;
+const Conatiner = styled.div`
+  display: flex;
+`;
+const CodeArea = styled.textarea`
+  margin-left: 2rem;
+  font-size: 20px;
+  flex: 2;
+`;
+const OutputArea = styled.div`
+  flex: 1;
+  margin: 0 20px;
+  padding: 5px;
+  border: 1px solid black;
+`;
+const SubmitButton = styled.button`
+  margin: 1rem 2rem;
+  padding: 0.5rem 2rem;
+  font-size: 16px;
+  border: none;
+  cursor: pointer;
+`;
 function App() {
-  return <></>;
+  return (
+    <>
+      <Title>Code Playground</Title>
+      <Conatiner>
+        <CodeArea rows="22" cols="100"></CodeArea>
+        <OutputArea></OutputArea>ˀ
+      </Conatiner>
+      <SubmitButton>Run</SubmitButton>
+    </>
+  );
 }
 
 export default App;
